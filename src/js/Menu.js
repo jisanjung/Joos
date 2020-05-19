40bf894df7445e81e89e2a7baa0384beed47d851
Jisan Jung <jjjung13@gmail.com>
2020-05-18 22:30:32 -0400

    organized js files

--- src/js/Menu.js
+++ src/js/Menu.js
@@ -1,12 +1,32 @@
+//variables
+const hamburger = document.querySelector(".hamburger");
+const mobileNav = document.querySelector(".mobile-nav");
+
+// open or close the mobile nav
+function nav(state) {
+    if (state) {
+        mobileNav.style.right = "0px";
+    } else if (!state) {
+        mobileNav.style.right = "-236px";
+    }
+}
+
 export class Menu {
     constructor(type) {
         this.type = type;
     }
 
-    getType() {
-        return this.type;
-    }
-    setType(type) {
-        this.type = type;
+    toggleMenu() {
+        hamburger.classList.add(this.type);
+
+        hamburger.addEventListener("click", function() {
+            if (this.classList.contains("is-active")) {
+                this.classList.remove("is-active");
+                nav(false);
+            } else {
+                this.classList.add("is-active");
+                nav(true);
+            }
+        });
     }
 }
--- src/js/main.js
+++ src/js/main.js
@@ -1,14 +1,5 @@
 import { Menu } from "./Menu.js";
 
+// header
 let m = new Menu("hamburger--spin-r");
-const hamburger = document.querySelector(".hamburger");
-
-hamburger.classList.add(m.getType());
-
-hamburger.addEventListener("click", function() {
-    if (this.classList.contains("is-active")) {
-        this.classList.remove("is-active");
-    } else {
-        this.classList.add("is-active");
-    }
-});
+m.toggleMenu();
