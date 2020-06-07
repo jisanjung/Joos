5604f52efd14affbe50f62f835cdab3c7fcc0b8c
Jisan Jung <jjjung13@gmail.com>
2020-06-07 13:58:34 -0400

    map initalized

--- src/js/main.js
+++ src/js/main.js
@@ -9,3 +9,11 @@ let rellax = new Rellax(".rellax");
 
 // animate on scroll
 AOS.init();
+
+// leaflet map 
+var mymap = L.map('mapid').setView([40.71, -74.00], 10);
+L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+attribution: '&copy; <a href="http://osm.org/           copyright">OpenStreetMap</a> contributors'
+}).addTo(mymap);
+var marker = L.marker([40.71, -74.00]).addTo(mymap);
+marker.bindPopup("New York, New York").openPopup();
